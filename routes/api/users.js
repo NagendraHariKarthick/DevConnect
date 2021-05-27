fb3f2b5bca77056c4b4a823f1fb2ae7cad169182
Nagendra Hari Karthick <nagendraharikarthick@gmail.com>
2021-05-27 08:06:52 +0530

    User model name changed

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,28 +7,27 @@ const config = require('config');
 const { check, validationResult } = require('express-validator');
 const normalize = require('normalize-url');
 
-const User = require('../../models/Users');
-
+const User = require('../../models/User');
 
 // @route GET api/users
 // @desc Register User
 // @access Public
 
-router.post('/',
-[
+router.post(
+  '/',
+  [
     check('name', 'Name is required').notEmpty(),
     check('email', 'Please include a valid email').isEmail(),
     check(
       'password',
       'Please enter a password with 6 or more characters'
-    ).isLength({ min: 6 })
-],
-async (req,res)=>{
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array()})
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-
 
     const { name, email, password } = req.body;
 
@@ -45,7 +44,7 @@ async (req,res)=>{
         gravatar.url(email, {
           s: '200',
           r: 'pg',
-          d: 'mm'
+          d: 'mm',
         }),
         { forceHttps: true }
       );
@@ -54,7 +53,7 @@ async (req,res)=>{
         name,
         email,
         avatar,
-        password
+        password,
       });
 
       const salt = await bcrypt.genSalt(10);
@@ -65,8 +64,8 @@ async (req,res)=>{
 
       const payload = {
         user: {
-          id: user.id
-        }
+          id: user.id,
+        },
       };
 
       jwt.sign(
